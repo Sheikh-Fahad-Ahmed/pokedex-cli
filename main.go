b32c3d086c72b2d78fcc7d96f39faa335a56c878
Fahad Ahmed <fahad838@gmail.com>
2025-06-28 17:29:45 +0530

    Add a scanner for I/O and a registry of commands

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
+type cliCommand struct {
+	name string
+	description string
+	callback func() error
+}
+
+var commands = map[string]cliCommand {
+	"exit":{
+		name : "exit",
+		description: "Exit the Pokedex",
+		callback: commandExit,
+	},
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() {
+			line := scanner.Text()
+			cleanedLine := cleanInput(line)
+			
+		}
+
+	}
 }
 
 func cleanInput(text string) []string {
@@ -16,7 +42,7 @@ func cleanInput(text string) []string {
 			if len(word) == 0 {
 				continue
 			}
-			words = append(words, word)
+			words = append(words, strings.ToLower(word))
 			word = ""
 			continue
 		}
@@ -26,4 +52,11 @@ func cleanInput(text string) []string {
 		words = append(words, word)
 	}
 	return words
+}
+
+// All Command Functions
+
+func commandExit() {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
 }
