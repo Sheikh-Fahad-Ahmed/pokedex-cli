93b0d99776cf0accca60291d172549fc6d4b5a47
Fahad Ahmed <fahad838@gmail.com>
2025-07-01 21:38:52 +0530

    Create cache.go file for caching api calls

---
+++ internal/pokecache/cache.go
@@ -0,0 +1,35 @@
+package cache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	data map[string]cacheEntry
+	mu   sync.Mutex
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func NewCache(interval time.Duration) *Cache {
+	new_cache := &Cache{
+		data: make(map[string]cacheEntry),
+	}
+	return new_cache
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.data[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+
+}
+
